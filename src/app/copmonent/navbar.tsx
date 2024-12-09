34c5f9652c1ee3256d925a99d36b91c0acd941df
sanaullah023 <su7341234@gmail.com>
2024-12-09 14:56:12 +0500

    Initial commit

---
+++ src/app/copmonent/navbar.tsx
@@ -0,0 +1,44 @@
+
+// import Link from 'next/link'; // Importing Link from next/link
+
+// const Navbar = () => {
+//   return (
+//     <div className="bg-[#FBEBB5] w-[1440px] h-[100px] flex justify-between items-center">
+
+//       {/* Navbar Links */}
+//       <span className="text-[#000000] w-[430px] h-[24px] mt-[10px] ml-[505px] flex justify-center items-center space-x-10">
+//         <Link href="/home" className="text-xl">Home</Link>
+//         <Link href="/shop" className="text-xl">Shop</Link>
+//         <Link href="/about" className="text-xl">About</Link>
+//         <Link href="/contact" className="text-xl">Contact</Link>
+//       </span>
+
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+
+
+import Link from 'next/link'; // Importing Link from next/link
+
+const Navbar = () => {
+  return (
+    <div className="bg-[#FBEBB5] w-[1440px] h-[100px] flex justify-between items-center">
+
+      {/* Navbar Links */}
+      <span className="text-[#000000] w-[430px] h-[24px] mt-[10px] ml-[505px] flex justify-center items-center space-x-10">
+        <Link href="/home" className="text-xl">Home</Link>
+        <Link href="/shop" className="text-xl">Shop</Link>
+        <Link href="/about" className="text-xl">About</Link>
+        <Link href="/contact" className="text-xl">Contact</Link>
+      </span>
+
+
+
+    </div>
+  );
+};
+
+export default Navbar;
---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+
+import Navbar from "./copmonent/navbar";
+import Sofa from "./copmonent/sofa";
+
+
+export default function Home() {
+  return(
+    <div>
+      <Navbar/>
+      <Sofa/>
+     
+    </div>
+  );
+}
